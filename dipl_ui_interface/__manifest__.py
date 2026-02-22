94ad4593f803ebde07c1e6699f4c9995aae867e5
Santiago Migoni <santiagomigoni@gmail.com>
2026-02-22 20:25:57 -0300

    feat(ui): primer commit, eliminación módulo anterior, nuevo módulo de interfaz visual

---
+++ dipl_ui_interface/__manifest__.py
@@ -0,0 +1,14 @@
+{ 
+    'name': 'dipl_ui_interface', 
+    'version': '1.0', 
+    'category': 'Custom', 
+    'author': 'Dipleg', 
+    'depends': ['base'], 
+    'data': [ 
+        'security/ir.model.access.csv', 
+        'views/views.xml', 
+    ], 
+    'installable': True, 
+    'application': False, 
+    'license': 'LGPL-3', 
+}
