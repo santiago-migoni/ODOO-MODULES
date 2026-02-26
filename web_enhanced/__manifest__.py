acb80919e10436eca6b7ddf5feed6fc2e6730dd1
Santiago Migoni <santiagomigoni@gmail.com>
2026-02-26 08:31:06 -0300

    feat(web_enhanced): crear estructura nuevo modulo, copiar modulo de tercero a mejorar

---
+++ web_enhanced/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'web_enhanced',
+    'version': '1.0',
+    'category': 'Dipleg/Technical',
+    'summary': 'Módulo personalizado para Technical',
+    'author': 'Dipleg',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
