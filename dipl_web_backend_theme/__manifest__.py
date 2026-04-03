08f4ece3a5b8fc0d4a623351b146fb976c22a68f
Santiago Migoni <santiagomigoni@gmail.com>
2026-04-03 17:08:21 -0300

    [IMP] sprint 1 shell and base visual refinement

--- dipl_web_backend_theme/__manifest__.py
+++ dipl_web_backend_theme/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Dipleg Web Backend Theme",
-    "version": "19.0.1.2.0",
+    "version": "19.0.1.3.0",
     "summary": "Dipleg backend theme for Odoo Community",
     "category": "Hidden",
     "author": "Dipleg S.A.",
