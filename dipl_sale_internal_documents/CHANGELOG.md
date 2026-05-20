e99cf16a0c7c48d71f72a08e6731c5de33234730
Santiago Migoni <santiagomigoni@gmail.com>
2026-05-20 11:55:39 -0300

    feat: add dipl_sale_internal_documents with cutting and internal order reports

---
+++ dipl_sale_internal_documents/CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+## Unreleased
+
+### Added
+- Initial Odoo 19 scaffolding for `dipl_sale_internal_documents` as a Dipleg-owned internal sales document module.
+- New `Cutting List` PDF report action for `sale.order`.
+- New `Internal Sales Order` PDF report action for `sale.order`.
+- Internal report templates that reuse the same documentary family as `dipl_doc_sale`.
+- Technical line rendering based on `dipl_sale_technical_quote` fields on `sale.order.line`.
---
+++ dipl_sale_internal_documents/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Dipleg Sales Internal Documents",
+    "summary": "Internal cutting and administrative sales documents",
+    "version": "19.0.1.0.0",
+    "category": "Dipleg",
+    "license": "LGPL-3",
+    "author": "Dipleg",
+    "depends": [
+        "sale_management",
+        "dipl_sale_technical_quote",
+        "l10n_ar",
+    ],
+    "data": [
+        "report/internal_document_actions.xml",
+        "report/internal_document_templates.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
